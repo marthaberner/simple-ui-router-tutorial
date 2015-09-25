482701a33521709b1927900e359b60616c7682d1
marthaberner <marthaberner.mb@gmail.com>
2015-09-24 23:43:09 -0600

    fix typos and minor tweak authentication func

--- services/authentication.js
+++ services/authentication.js
@@ -2,7 +2,7 @@ app.factory('AuthService', function () {
   return {
     isAuthenticated: function () {
       var num = Math.floor((Math.random() * 10) + 1);
-      return num > 5;
+      return num === 3 || num === 7;
     }
   }
 })
